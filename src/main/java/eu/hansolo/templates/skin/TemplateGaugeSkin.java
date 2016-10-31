21df501421ff8e0b8203b9bb7280f1ce4b6fa19b
han.solo <han.solo@mac.com>
2016-10-31 16:01:11 +0100

    Added templates for Clock and Gauge skins

---
+++ src/main/java/eu/hansolo/templates/skin/TemplateGaugeSkin.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2016 by Gerrit Grunwald
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.hansolo.templates.skin;
+
+import eu.hansolo.medusa.Gauge;
+import javafx.geometry.Insets;
+import javafx.scene.control.Skin;
+import javafx.scene.control.SkinBase;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+
+
+/**
+ * Created by hansolo on 31.10.16.
+ */
+public class TemplateGaugeSkin extends SkinBase<Gauge> implements Skin<Gauge> {
+    private static final double PREFERRED_WIDTH  = 250;
+    private static final double PREFERRED_HEIGHT = 250;
+    private static final double MINIMUM_WIDTH    = 50;
+    private static final double MINIMUM_HEIGHT   = 50;
+    private static final double MAXIMUM_WIDTH    = 1024;
+    private static final double MAXIMUM_HEIGHT   = 1024;
+    private              double aspectRatio      = PREFERRED_HEIGHT / PREFERRED_WIDTH;
+    private              double width;
+    private              double height;
+    private              double size;
+    private              Pane   pane;
+
+
+    // ******************** Constructors **************************************
+    public TemplateGaugeSkin(Gauge gauge) {
+        super(gauge);
+        if (gauge.isAutoScale()) gauge.calcAutoScale();
+
+        initGraphics();
+        registerListeners();
+    }
+
+
+    // ******************** Initialization ************************************
+    private void initGraphics() {
+        // Set initial size
+        if (Double.compare(getSkinnable().getPrefWidth(), 0.0) <= 0 || Double.compare(getSkinnable().getPrefHeight(), 0.0) <= 0 ||
+            Double.compare(getSkinnable().getWidth(), 0.0) <= 0 || Double.compare(getSkinnable().getHeight(), 0.0) <= 0) {
+            if (getSkinnable().getPrefWidth() > 0 && getSkinnable().getPrefHeight() > 0) {
+                getSkinnable().setPrefSize(getSkinnable().getPrefWidth(), getSkinnable().getPrefHeight());
+            } else {
+                getSkinnable().setPrefSize(PREFERRED_WIDTH, PREFERRED_HEIGHT);
+            }
+        }
+
+        pane = new Pane();
+        pane.setBorder(new Border(new BorderStroke(getSkinnable().getBorderPaint(), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+        pane.setBackground(new Background(new BackgroundFill(getSkinnable().getBackgroundPaint(), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        getChildren().setAll(pane);
+    }
+
+    private void registerListeners() {
+        getSkinnable().widthProperty().addListener(o -> handleEvents("RESIZE"));
+        getSkinnable().heightProperty().addListener(o -> handleEvents("RESIZE"));
+        getSkinnable().setOnUpdate(e -> handleEvents(e.eventType.name()));
+    }
+
+
+    // ******************** Methods *******************************************
+    @Override protected double computeMinWidth(final double HEIGHT, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT)  { return MINIMUM_WIDTH; }
+    @Override protected double computeMinHeight(final double WIDTH, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT)  { return MINIMUM_HEIGHT; }
+    @Override protected double computePrefWidth(final double HEIGHT, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT) { return super.computePrefWidth(HEIGHT, TOP, RIGHT, BOTTOM, LEFT); }
+    @Override protected double computePrefHeight(final double WIDTH, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT) { return super.computePrefHeight(WIDTH, TOP, RIGHT, BOTTOM, LEFT); }
+    @Override protected double computeMaxWidth(final double HEIGHT, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT)  { return MAXIMUM_WIDTH; }
+    @Override protected double computeMaxHeight(final double WIDTH, final double TOP, final double RIGHT, final double BOTTOM, final double LEFT)  { return MAXIMUM_HEIGHT; }
+
+    private void handleEvents(final String EVENT_TYPE) {
+        if ("RESIZE".equals(EVENT_TYPE)) {
+            resize();
+            redraw();
+        } else if ("REDRAW".equals(EVENT_TYPE)) {
+            redraw();
+        } else if ("VISIBILITY".equals(EVENT_TYPE)) {
+            redraw();
+        } else if ("RECALC".equals(EVENT_TYPE)) {
+            redraw();
+        } else if ("SECTIONS".equals(EVENT_TYPE)) {
+            redraw();
+        }
+    }
+
+
+    // ******************** Resizing ******************************************
+    private void resize() {
+        width  = getSkinnable().getWidth() - getSkinnable().getInsets().getLeft() - getSkinnable().getInsets().getRight();
+        height = getSkinnable().getHeight() - getSkinnable().getInsets().getTop() - getSkinnable().getInsets().getBottom();
+        size   = width < height ? width : height;
+
+        if (aspectRatio * width > height) {
+            width = 1 / (aspectRatio / height);
+        } else if (1 / (aspectRatio / height) > width) {
+            height = aspectRatio * width;
+        }
+
+        if (width > 0 && height > 0) {
+            // Use for square controls where width == height
+            pane.setMaxSize(size, size);
+            pane.setPrefSize(size, size);
+            pane.relocate((getSkinnable().getWidth() - size) * 0.5, (getSkinnable().getHeight() - size) * 0.5);
+
+            // Use for rectangular controls width != height
+            pane.setMaxSize(width, height);
+            pane.setPrefSize(width, height);
+            pane.relocate((getSkinnable().getWidth() - width) * 0.5, (getSkinnable().getHeight() - height) * 0.5);
+
+            // Resize and relocate elements here
+
+        }
+    }
+
+    private void redraw() {
+        pane.setBorder(new Border(new BorderStroke(getSkinnable().getBorderPaint(), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(getSkinnable().getBorderWidth() / PREFERRED_WIDTH * height))));
+        pane.setBackground(new Background(new BackgroundFill(getSkinnable().getBackgroundPaint(), CornerRadii.EMPTY, Insets.EMPTY)));
+    }
+}
+
